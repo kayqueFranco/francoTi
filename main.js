cf0ec7c90c5337a860c5d8c2f84339dad50702cd
kayqueFranco <kayque.franco2018@gmail.com>
2025-02-17 17:14:36 -0300

    Inicio de nova uc

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("Processo principal")
+
+
+const { app, BrowserWindow, nativeTheme } = require('electron')
+//Janela principal
+let win
+const createWindow = () => {
+    // a linha abaixo define o tema claro ou escuro
+    nativeTheme.themeSource = 'light' //(Dark ou  light)
+   win = new BrowserWindow({
+        width: 800,
+        height: 600,
+      //  autoHideMenuBar: true,
+       // minimizable: false,
+        resizable: false,
+
+  })
+
+  win.loadFile('./src/views/index.html')
+}
+//inicial a aplicação 
+app.whenReady().then(() => {
+  createWindow()
+})
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+  
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
